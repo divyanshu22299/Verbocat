8f4b239477172909e1c95e6d987df835a76439ff
divyanshu22299 <divyanshusingh2229@gmail.com>
2026-05-21 15:20:22 +0530

    fixed production api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://verbocat.onrender.com"
 });
 
 export const uploadFile = async (file) => {
